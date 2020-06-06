e3e6dcd8cd91478bff8843dd074b50b8fdb2b65a
suraj bose <suraj.bose@au.ey.com>
2020-06-06 18:13:05 +1000

    implement search and paging

--- NetCoreAPI/Controllers/TestController.cs
+++ NetCoreAPI/Controllers/TestController.cs
@@ -8,11 +8,21 @@ namespace NetCoreAPI.Controllers
     public class TestController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string sortOrder)
         {
+            switch (sortOrder)
+            {
+                case "asc":
+                    //apply sort by ascending
+                    break;
+                case "desc":
+                    //apply sort by descending
+                    break;
+                default:
+                    break;
+            }
             return Ok("Data OK");
         }
-
 
         [HttpGet("{id:int}",Name ="Get")]
         public IActionResult GetById(int id)
@@ -32,6 +42,26 @@ namespace NetCoreAPI.Controllers
             return Ok("Data OK " + id);
         }
 
+        /// <summary>
+        /// Implement paging
+        /// </summary>
+        /// <param name="pageSize"></param>
+        /// <param name="pageNumber"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult PagingData(int pageSize, int pageNumber)
+        {
+            //var pagedData = data.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+            return Ok("paged data");
+        }
 
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult SearchData(string parameter)
+        {
+            //var filterData = data.Where(x=>x.Col.StartsWith(parameter));
+            return Ok("filter data");
+        }
     }
 }
