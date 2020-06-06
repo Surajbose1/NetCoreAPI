51857837223d7c198c9dc98a8f856ba06a19b22f
suraj bose <suraj.bose@au.ey.com>
2020-06-06 17:38:53 +1000

    HTTP Get with name

---
+++ NetCoreAPI/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace NetCoreAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Data OK");
+        }
+
+
+        [HttpGet("{id:int}",Name ="Get")]
+        public IActionResult GetById(int id)
+        {
+            return Ok("Data OK " + id);
+        }
+
+        [HttpGet("{id:alpha}", Name = "Get1")]
+        public IActionResult GetByIdString(string id)
+        {
+            return Ok("Data OK " + id);
+        }
+
+        [HttpGet("{id:guid}", Name = "Get2")]
+        public IActionResult GetByIdString(Guid id)
+        {
+            return Ok("Data OK " + id);
+        }
+
+
+    }
+}
